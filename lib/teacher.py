24167ca28e373c1a54d445bd715a48ca57543762
Ali1-coder <ablhur3@gmail.com>
2025-03-11 16:42:36 +0300

    All test pass

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,20 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first, last):
+        super().__init__(first, last)
+        self.knowledge=["str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast"]
 
     def teach(self):
-        pass
+        if self.knowledge:
+            index = random.randint(0, len(self.knowledge) - 1)
+            return self.knowledge[index]
+        else:
+            return None
